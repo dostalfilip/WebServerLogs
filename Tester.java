956f48b149a3bccee90c9ea6b43e34c9b0a087d0
Filip Dostal <dostal.filip@hotmail.com>
2016-06-01 12:36:59 +0800

    testing

--- Tester.java
+++ Tester.java
@@ -20,13 +20,27 @@ public class Tester
     private static void testUniqueIPAnalyzer() {
         LogAnalyzer test = new LogAnalyzer();
         //test.readFile("./Data/short-test_log");
-        test.readFile("./Data/weblog3-short_log");
-    
-      //  HashMap<String,Integer> myMap = test.countVisitsPerIP();
-      //  for(String n : myMap.keySet()){
-      //  	System.out.println(n+" Zobrazeno: "+myMap.get(n));
-      //  }
-    
+        //test.readFile("./Data/weblog3-short_log");
+        test.readFile("./Data/weblog2_log");
+        //System.out.print(test.countUniqueIPs());
+        //System.out.print(test.uniqueIPVisitsOnDay("Sep 27").size());
+        //System.out.print(test.countUniqueIPsInRange(400,499) );
+        
+       /* 
+        HashMap<String,Integer> myMap = test.countVisitsPerIP();
+       for(String n : myMap.keySet()){
+        	System.out.println(n+" Zobrazeno: "+myMap.get(n));
+        }
+       System.out.print(test.mostNumberVisitsByIP(myMap) );
+    */
+      /*  HashMap<String,Integer> myMap = test.countVisitsPerIP();
+          System.out.print(test.iPsMostVisits (myMap) );
+      */  
+        
+      //  HashMap<String,ArrayList<String>> myMap = test.iPsForDays();
+       // System.out.print(test.dayWithMostIPVisits(myMap) );
+        
+        
       //  System.out.println(test.mostNumberVisitsByIP(test.countVisitsPerIP()));
         
         
@@ -44,16 +58,16 @@ public class Tester
         }
         */
         
-        //System.out.print(test.dayWithMostIPVisits(test.iPsForDays()));
+        System.out.print(test.dayWithMostIPVisits(test.iPsForDays()));
         
         
         /*
          * Test last method iPsWithMostVisitsOnDay
          */
         
-        for (String n : test.iPsWithMostVisitsOnDay(test.iPsForDays(), "Sep 30")){
-        	System.out.println(n);
-        }
+       // for (String n : test.iPsWithMostVisitsOnDay(test.iPsForDays(), "Sep 30")){
+      //  	System.out.println(n);
+       // }
         
         
         
